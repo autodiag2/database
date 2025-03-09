7f9fd18646888c2659300be6ecc3c1d904e0ce1d
ffprivacy <ffprivacy@netcourrier.com>
2025-03-09 22:18:29 +0100

    VIN manufacturers

---
+++ scripts/vin_manufacturers_opencars_vin_decoder_api.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# URL of the manufacturers CSV file
+URL="https://raw.githubusercontent.com/opencars/vin-decoder-api/refs/heads/master/manufacturers.csv"
+LOCAL_FILE="data/VIN/manufacturers.tsv"
+TEMP_FILE="data/VIN/manufacturers_temp.tsv"
+
+# Download the CSV file
+curl -s "$URL" | awk -F';' '1 { gsub(/"/, "", $1); gsub(/"/, "", $2); print $1 "\t" $2 }' > "$TEMP_FILE"
+
+# Check if the file was downloaded successfully
+if [ -s "$TEMP_FILE" ]; then
+    tail -n +2 "$TEMP_FILE" >> "$LOCAL_FILE"
+    sort -u "$LOCAL_FILE" -o "$LOCAL_FILE"
+    echo "Updated $LOCAL_FILE with new entries."
+else
+    echo "Failed to download or process the CSV file."
+fi
+rm -f "$TEMP_FILE"
