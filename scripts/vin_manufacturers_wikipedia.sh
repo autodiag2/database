e58d1469ff193016ecd595d1b2f03edaa817e600
ffprivacy <ffprivacy@netcourrier.com>
2025-03-10 15:35:59 +0100

    priorise data from wikipedia because manufacturer sub part

--- scripts/vin_manufacturers_wikipedia.sh
+++ scripts/vin_manufacturers_wikipedia.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# https://en.wikipedia.org/wiki/Vehicle_identification_number
 LOCAL_FILE="data/VIN/manufacturers.tsv"
-cat "scripts/wikipedia_sae_wmi_manufacturers.tsv" >> "$LOCAL_FILE"
-sort -k1,1 -u "$LOCAL_FILE" -o "$LOCAL_FILE"
+cat "scripts/wikipedia_sae_wmi_manufacturers.tsv" >> .temp
+cat "$LOCAL_FILE" >> .temp
+sort -k1,1 -u .temp -o "$LOCAL_FILE"
+rm .temp
 echo "Updated $LOCAL_FILE with new entries."
